082880590999979997e2ba4403f654e796c2d9ea
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-27 18:50:20 +0330

    create user module

---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("user")
+export class UserEntity {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column()
+  mobile: string;
+
+  @Column()
+  mobile_verified: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
