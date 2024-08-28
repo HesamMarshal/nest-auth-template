cf0fb539af312af44497b84cea154e8e9baea1af
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-28 18:10:30 +0330

    remove a space

--- src/modules/auth/dto/auth.dto.ts
+++ src/modules/auth/dto/auth.dto.ts
@@ -1,11 +1,11 @@
 import { IsMobilePhone, IsString, Length } from "class-validator";
 
 export class SendOtpDto {
-  @IsMobilePhone("fa-IR", {}, { message: " Mobile is not in correct format" })
+  @IsMobilePhone("fa-IR", {}, { message: "Mobile is not in correct format" })
   mobile: string;
 }
 export class CheckOtpDto {
-  @IsMobilePhone("fa-IR", {}, { message: " Mobile is not in correct format" })
+  @IsMobilePhone("fa-IR", {}, { message: "Mobile is not in correct format" })
   mobile: string;
   @IsString()
   @Length(5, 5, { message: "Incorrect code" })
