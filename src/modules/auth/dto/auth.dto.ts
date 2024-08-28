fd5d74a6986d7774bf3755deef792723506bb172
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-28 12:20:09 +0330

    create SendOtpDto, CheckOtpDto

---
+++ src/modules/auth/dto/auth.dto.ts
@@ -0,0 +1,13 @@
+import { IsMobilePhone, IsString, Length } from "class-validator";
+
+export class SendOtpDto {
+  @IsMobilePhone("fa-IR", {}, { message: " Mobile is not in correct format" })
+  mobile: string;
+}
+export class CheckOtpDto {
+  @IsMobilePhone("fa-IR", {}, { message: " Mobile is not in correct format" })
+  mobile: string;
+  @IsString()
+  @Length(5, 5, { message: "Incorrect code" })
+  code: string;
+}
