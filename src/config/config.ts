93352dcf76aaf865ee0cfc8094fd12a11687d3d7
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-28 16:36:10 +0330

    create jwt secrets

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,13 +3,13 @@ import { registerAs } from "@nestjs/config";
 export enum ConfigKeys {
   App = "App",
   Db = "Db",
+  Jwt = "Jwt",
 }
 
-const AppConfig = registerAs(ConfigKeys.App, () => {
-  return {
-    port: 3000,
-  };
-});
+const AppConfig = registerAs(ConfigKeys.App, () => ({
+  port: 3000,
+}));
+
 const DbConfig = registerAs(ConfigKeys.Db, () => {
   return {
     port: 5432,
@@ -20,4 +20,9 @@ const DbConfig = registerAs(ConfigKeys.Db, () => {
   };
 });
 
-export const configurations = [AppConfig, DbConfig];
+const JwtConfig = registerAs(ConfigKeys.Jwt, () => ({
+  accessTokenSecret: "0f4d1684aec62727d350386415e0f3f35849690a",
+  refreshTokenSecret: "7d60febfd9c79bf45ec6126f14dfe69a57444099",
+}));
+
+export const configurations = [AppConfig, DbConfig, JwtConfig];
