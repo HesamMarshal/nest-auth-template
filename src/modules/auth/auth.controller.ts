4b177e84bf83eec4d58bba41ba9270659bda57e1
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-28 12:22:20 +0330

    create routes for /send-otp, /check-otp

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Body, Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { CheckOtpDto, SendOtpDto } from "./dto/auth.dto";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post("/send-otp")
+  sendOTP(@Body() otpDto: SendOtpDto) {
+    return this.authService.sendOTP(otpDto);
+  }
+
+  @Post("/check-otp")
+  checkOTP() {}
 }
